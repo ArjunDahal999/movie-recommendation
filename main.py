b0e413484b8d0dc96bc3de68f6de7c5c3c0ecae2
ArzunDhz <86089364+ArzunDhz@users.noreply.github.com>
2024-09-04 07:54:35 +0545

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import re
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -19,6 +21,7 @@ app.add_middleware(
 
 # Load movie data once at startup
 movie_data = pd.read_csv("movies.csv", encoding="utf-8")
+movie_data_file = pd.read_csv("movies.csv", encoding="utf-8")
 movie_data = movie_data.fillna({
     'budget': 0,
     'genres': 'Unknown',
@@ -46,6 +49,7 @@ movie_data = movie_data.fillna({
 selected_features = ['genres', 'keywords', 'tagline', 'cast', 'director']
 
 
+
 combined_features = movie_data[selected_features].agg(' '.join, axis=1)
 
 # Create TF-IDF vectorizer and similarity matrix
@@ -64,9 +68,11 @@ def predict_movies(movie: str, top_n: int = 15):
     sorted_similar_movies = sorted(similarity_score, key=lambda x: x[1], reverse=True)
     return [movie_data.iloc[movie[0]]['title'] for movie in sorted_similar_movies[1:top_n+1]]
 
+
 @app.get('/')
 def home():
     return {"status": "Working"}
+
 
 @app.get('/predict/{moviename}')
 def predict_movies_endpoint(moviename: str):
@@ -74,8 +80,19 @@ def predict_movies_endpoint(moviename: str):
 
 @app.get('/random-movie')
 def random_movie():
-    random_movies = movie_data.sample(n=10)[['budget', 'title', 'director', 'release_date', 'popularity']]
+    random_movies = movie_data.sample(n=15)[['revenue', 'title', 'director', 'release_date', 'popularity']]
     return {"data": random_movies.to_dict(orient="records")}
+
+@app.get('/top-popular-movies')
+def top_popular_movies():
+    top_movies = movie_data.nlargest(15, 'popularity')[['revenue', 'title', 'director', 'release_date', 'popularity']]
+    return {"data": top_movies.to_dict(orient="records")}
+
+@app.get('/highest-grossing-movie')
+def top_popular_movies():
+    top_movies = movie_data.nlargest(15, 'revenue')[['revenue', 'title', 'director', 'release_date', 'popularity']]
+    return {"data": top_movies.to_dict(orient="records")}
+
 
 @app.get('/autocomplete/{name}')
 def auto_suggestion(name: str):
